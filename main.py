4c4e9b610fe17c4b5faaf27ea98aa400dce7f8f4
Lou <marilounacional0717@gmail.com>
2025-06-24 09:08:38 +0800

    abstract_game_objects

--- main.py
+++ main.py
@@ -33,3 +33,37 @@ class NoteType(Enum):
     NORMAL = "NORMAL"
     HOLD = "HOLD"
     SPECIAL = "SPECIAL"
+
+
+# ABSTRACTION: Abstract base classes define interfaces
+class GameObject(ABC):
+    """Abstract base class for all game objects"""
+    def __init__(self, x, y):
+        self._x = x  # ENCAPSULATION: Private attributes
+        self._y = y
+        
+    @property
+    def x(self):  # ENCAPSULATION: Controlled access via properties
+        return self._x
+    
+    @x.setter
+    def x(self, value):
+        self._x = value
+        
+    @property
+    def y(self):
+        return self._y
+    
+    @y.setter
+    def y(self, value):
+        self._y = value
+    
+    @abstractmethod
+    def update(self):
+        """Abstract method that must be implemented by subclasses"""
+        pass
+    
+    @abstractmethod
+    def draw(self, screen):
+        """Abstract method for drawing the object"""
+        pass
