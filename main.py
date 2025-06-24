ce30e1443ac367c77176cf7390d74631c351623a
Lou <marilounacional0717@gmail.com>
2025-06-24 08:57:39 +0800

    screen

--- main.py
+++ main.py
@@ -8,3 +8,18 @@ from enum import Enum
 pygame.init()
 pygame.mixer.init()
 
+# Constants
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+FPS = 60
+
+# Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+PURPLE = (255, 0, 255)
+ORANGE = (255, 165, 0)
+
